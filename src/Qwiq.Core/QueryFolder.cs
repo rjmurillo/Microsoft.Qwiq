3b4a17902626eda74169a8c5e827c7c95ed38d32
Richard Murillo <rimuri@microsoft.com>
2017-10-03 12:59:01 -0700

    Correction for culture invariance
    Existing implementation created a string then called `ToString` on that string, which will not perform the culture transformation as expected. Keeping the result as a native formattable string will ensure its conversion to a culture invariant string for display.

--- src/Qwiq.Core/QueryFolder.cs
+++ src/Qwiq.Core/QueryFolder.cs
@@ -48,7 +48,8 @@ namespace Microsoft.Qwiq
 
         public override string ToString()
         {
-            return $"{Id} ({Name})".ToString(CultureInfo.InvariantCulture);
+            FormattableString s = $"{Id} ({Name})";
+            return s.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
